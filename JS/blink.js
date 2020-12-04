3589a1b5bfa11ab78dd317b4bd02dba66855b24c
NilsB2911 <63862156+NilsB2911@users.noreply.github.com>
2020-12-04 22:35:35 +0100

    idk

--- JS/blink.js
+++ JS/blink.js
@@ -1,40 +1,30 @@
 window.onload = function () {
     let o = true;
 
-    document.getElementById('aboutMe').onmouseenter = function () {
-        o = true;
-        setInterval(changeBlink, 500);
-        console.log(o)
-    };
+    document.getElementById('aboutMe').addEventListener("mouseenter", startBlink);
+    document.getElementById('aboutMe').addEventListener("mouseleave", stopBlink);
 
-    /*document.getElementById("work").onmouseover = function () {
-        o = true;
-        setInterval(changeBlink, 500);
-        console.log(o)
-    };
-
-    document.getElementById("connect").onmouseover =  function () {
-        o = true;
+    function startBlink() {
         setInterval(changeBlink, 500);
         console.log(o);
-    };*/
+    }
 
     function changeBlink() {
         if (o) {
             document.getElementById('c1').style.opacity = 1;
             o = false;
-            console.log(o)
+            console.log(o);
         } else {
             document.getElementById('c1').style.opacity = 0;
             o = true;
-            console.log(o)
+            console.log(o);
         }
     };
-};
 
-document.getElementById('aboutMe').onmouseleave = function () {
-    document.getElementById('c1').style.opacity = 0;
-    delete o;
+    function stopBlink() {
+        ;
+    };
 }
+
 
 
