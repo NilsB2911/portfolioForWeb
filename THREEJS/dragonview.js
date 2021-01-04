a1c92a47b598fe5932627c9adbde4489bd189181
NilsB2911 <63862156+NilsB2911@users.noreply.github.com>
2021-01-04 22:03:18 +0100

    idk what i did

---
+++ THREEJS/dragonview.js
@@ -0,0 +1,68 @@
+//Variables for setup
+
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+import * as OBJLoader from '../THREEJS/loaders/OBJLoader';
+
+let container;
+let camera;
+let renderer;
+let scene;
+let house;
+
+function init() {
+    container = document.querySelector(".scene");
+    console.log(container);
+
+    //Create scene
+    scene = new THREE.Scene();
+
+    const fov = 35;
+    const aspect = container.clientWidth / container.clientHeight;
+    const near = 0.1;
+    const far = 1000;
+
+    //Camera setup
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 5, 30);
+
+    const ambient = new THREE.AmbientLight(0x404040, 2);
+    scene.add(ambient);
+
+    const light = new THREE.DirectionalLight(0xffffff, 2);
+    light.position.set(50, 50, 100);
+    scene.add(light);
+    //Renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(container.clientWidth, container.clientHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    console.log(renderer);
+
+    container.appendChild(renderer.domElement);
+
+    //Load Model
+    let loader = new THREE.OBJLoader();
+    loader.setPath('../THREEJS/');
+    loader.load("harrie.obj", function(obj) {
+        scene.add(obj.scene);
+        house = obj.scene.children[0];
+        animate();
+    });
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    house.rotation.z += 0.005;
+    renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(container.clientWidth, container.clientHeight);
+}
+
+window.addEventListener("resize", onWindowResize);
