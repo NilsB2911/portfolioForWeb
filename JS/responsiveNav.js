cbfaffc4591c2ea869de65c6f8f0f2445d783768
johannesdmeyer <mail-johannes-meyer@gmx.de>
2021-01-10 14:51:00 +0100

    commit

---
+++ JS/responsiveNav.js
@@ -0,0 +1,8 @@
+function openNav (){
+    document.getElementById("topbar").style.width = "25vh";
+    //document.getElementById("topbar").style.padding-left= "5vw";
+    //document.getElementById("topbar").style.border-left = "solid rgba(206, 206, 206, 0.384) 1px";
+}
+function closeNav (){
+    document.getElementById("topbar").style.width = "0vh";
+}
