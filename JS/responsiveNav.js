ee2b0ba1f95263e371b5c0deef300a72895e87c7
johannesdmeyer <mail-johannes-meyer@gmx.de>
2021-01-14 14:55:13 +0100

    Praise the navigation

--- JS/responsiveNav.js
+++ JS/responsiveNav.js
@@ -3,12 +3,12 @@ function openNav (){
     //document.getElementById("topbar").style.padding-left= "5vw";
     //document.getElementById("topbar").style.border-left = "solid rgba(206, 206, 206, 0.384) 1px";
 }
-document.getElementById("topbar").addEventListener("resize", function(){
+document.getElementById("topbar"), function(){
     let w = document.getElementById("topbar").offsetWidth;
     if (w >= 1020){
         document.getElementById("topbar").style.width = "9vh";
     }
 })
 function closeNav (){
-    document.getElementById("topbar").style.width = "0vh";
+    document.getElementById("topbar").style.width = "";
 }
